d4ccce2e4f9a1a32b66a6c37eafac2aed4babce6
RomeroNog <igorromeronigueira@gmail.com>
2024-10-04 10:31:14 -0300

    Cod de divisao

---
+++ divisoa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+    int a,b,teste=1;
+    float div;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d",&a);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d",&b);
+
+    div = (float)a/(float)b;
+    
+    printf("A divisao eh: %.2f",div);
+    
+return 0;
+}
