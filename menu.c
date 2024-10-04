4409c16ed726303d5bda038258065655fdddc732
cauansm8 <cauan.simoes.contato@gmail.com>
2024-10-04 11:21:50 -0300

    interface

---
+++ menu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    float a,b;
+
+    scanf ("%f",&a);
+    scanf ("%f",&b);
+
+    printf ("\nA soma: %f\n", a+b);
+    printf ("\nA subtracao: %f\n",a-b);
+    printf ("\nA multiplicacao: %f\n", a*b);
+    printf ("\nA divisao: %f\n",a/b);
+
+
+
+
+    return 0;
+}
